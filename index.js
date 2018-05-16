d73280370401568346e0ea8da095bbdb7326d1b6
Brenden Thornton <thorntonbrenden@gmail.com>
2018-05-16 19:03:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments){
+  var plays = [];
+  for(let i = 0; i < musicians.length; i++){
+    plays.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return plays;
+}
+
+function johnLennonFacts(facts){
+  var newFacts = [];
+  var i = 0;
+  while(i < facts.length){
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
